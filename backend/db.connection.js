d69f19b215d33983d4983054ba9c880f838c9160
ishikakhadka <khadkaishika123@gmail.com>
2025-06-19 11:57:16 +0545

    .env fixed

--- backend/db.connection.js
+++ backend/db.connection.js
@@ -6,15 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    // Debug environment variables
-    console.log("Environment Variables:");
-    console.log("DB_NAME:", process.env.DB_NAME);
-    console.log("DB_USER_NAME:", process.env.DB_USER_NAME);
-    console.log("DB_HOST:", process.env.DB_HOST);
-    console.log("DB_OPTION:", process.env.DB_OPTION);
-
     const url = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?${process.env.DB_OPTION}`;
-    console.log("Connection URL:", url);
 
     await mongoose.connect(url);
     console.log("Successfully connected to db");
--- ui/lib/axios.instance.ts
+++ ui/lib/axios.instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { access } from "fs";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 5000,
 });
 
